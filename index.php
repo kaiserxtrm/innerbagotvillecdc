9cf193b462ae42de9e3e77507427743530542445
kaiserxtrm <cummings.teddy@gmail.com>
2019-07-21 17:23:08 -0400

    Updated master template

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
         <meta charset="utf-8">
 
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
+        <!-- main css -->
+        <link rel="stylesheet" href="./css/main.css" >
+       
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -18,6 +21,8 @@
     <body>
     
         <header>
+
+        <!-- Navigation -->
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container">
                     <a class="navbar-brand" href="index.php">InnerBagotvilleCdc</a>
@@ -39,12 +44,23 @@
                     </div>
                 </div>
             </nav>
-   
-        </header>
 
+            <!-- Showcase -->
+            <div class="jumbotron">
+                <div class="container">
+                    <h1 class="display-4">Hello, world!</h1>
+                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr class="my-4">
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </div>
+            </div>
+        </header>
         <main>
-        
-        
+        <div class="block">
+            yes
+        </div>
+
         
         </main>
 
